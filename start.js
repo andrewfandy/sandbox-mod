55b6980169113563d268351ec15d53c42daa04a4
Biagio Santoro <slemeae@gmail.com>
2017-11-27 18:05:49 +0000

    Updated the whole script.

--- start.js
+++ start.js
@@ -1,17 +1,9 @@
 let _modPath;
-const rs = GetRootScope();
-rs.OpenMod = () => Remote.app.openLink('http://steamcommunity.com/sharedfiles/filedetails/stats/1109753463');
-rs.SendMail = () => GetRootScope().sendMail('[slem]',' Thank you for using Sandbox Mod!',
-`Hello!
-This is [slem], I want to thank you for using my mod.
 
-I will keep adding more stuff that is requested and suggested to the mod so keep tuned and leave some feedback if you want!
+// This is for opening the mod link in-game.
+GetRootScope().OpenMod = () => Remote.app.openLink('http://steamcommunity.com/sharedfiles/filedetails/stats/1109753463');
 
-If you have time could you please leave a rating to the mod? This can be done <a onclick="GetRootScope().OpenMod()">here</a>.
-
--Have fun playing, [slem]`);
-
-
+// I forgot what this does.
 const SettingsHelper = {
     getValueOrDefault: (settings, key, defaultEntry) => {
         if(!settings.sandbox) {
@@ -33,22 +25,24 @@ const SettingsHelper = {
         settings.sandbox[key] = value;
     }
 };
-Helpers.ConsoleInfo('[MOD] Sandbox Helper loaded!');
+
+// THis says the mod loaded in console.
+Helpers.ConsoleInfo('[MOD] Sandbox: Helper loaded!');
 
 
 exports.initialize = (modPath) =>{
     _modPath = modPath;
-
+    // Add a framework
     Frameworks.push(
-        {
-            order: 14,
-            name: 'pal',
-            licenseCost: 0,
-            maxFeatures: 42,
-            maxFeatureLevel: 40,
-        }
+      {
+        cuPerMs: 0.1,
+        licenseCost: 0,
+        name: 'Electron',
+        order: 0,
+        pricePerUser: 1
+      }
     );
-    Helpers.ConsoleInfo('[MOD] Sandbox Pal framework loaded!');
+    Helpers.ConsoleInfo('[MOD] Sandbox: Pal framework loaded!');
 
     // Add new menu item
     Modding.setMenuItem({
@@ -61,125 +55,62 @@ exports.initialize = (modPath) =>{
 
     // Define custom views
     exports.views = [
-        {
-            name: 'sandbox',
-            viewPath: _modPath + 'view.html',
-            controller: function ($rootScope) {
-                this.name = 'Jonas';
-                this.giveMoney = (amount) => {
-                  $rootScope.confirm('Are you sure?', `Are you sure you want ${numeral(amount).format(Configuration.CURRENCY_FORMAT)}?`, () => {
-                  $rootScope.settings.balance += amount
-                  $rootScope.addNotification("You 'borrowed' " + numeral(amount).format(Configuration.CURRENCY_FORMAT) + " !", 1);
-                  });
-                }
-
-                this.giveXP = (amount) => {
-                  $rootScope.confirm('Are you sure?', `Are you sure you want ${(amount)}XP ?`, () => {
-                    $rootScope.settings.xp += amount
-                    $rootScope.addNotification("Added " + amount + " XP!", 1);
-                  });
-                }
-
-                this.giveMaxTire = () => {
-                  $rootScope.confirm('Are you sure?', `Are you sure you want get max tire (xp) ?`, () => {
-                    Cheats.GetMaxTier()
-                    $rootScope.addNotification("Max tier given.", 1);
-
-                  });
-                }
-
-                this.NextDay = () => {
-                  $rootScope.confirm('Are you sure?', `Are you sure you want to skip a day?`, () => {
-                    Cheats.SkipADay()
-                    $rootScope.addNotification("You skipped one day.", 1);
-
-                  });
-                }
-
-                this.Hype = () => {
-                  $rootScope.confirm('Are you sure?', `Are you sure you want to add 100% hype to your projects.`, () => {
-                    _.forEach($rootScope.settings.products, (product) => {product.hype = 100;})
-                    $rootScope.addNotification("Added 100 hype to your products .", 1)
-                  });
-                }
-
-                this.addUsers = () => {
-                  $rootScope.confirm('Are you sure?', `Are you sure you want to add 500000 to <b>all</b> your projects.`, () => {
-                    _.forEach($rootScope.settings.products, (product) => {product.users.Mobile += 500000;})
-                    _.forEach($rootScope.settings.products, (product) => {product.users.Web += 500000;})
-                    _.forEach($rootScope.settings.products, (product) => {product.users.Desktop += 500000;})
-                    $rootScope.addNotification("Added 500000 users to your products.", 1);
-
-                  });
-                }
-
-                this.InventoryGive = () => {
-                  $rootScope.confirm('Are you sure?', `Are you sure you want to add 100 items of every item.`, () => {
-                    let e = GetRootScope();
-                    Components.forEach(t=>{
-                        e.settings.inventory[t.name] += 100;
-                    }
-                    )
-                    $rootScope.addNotification("Added 100 of every item to your inventory.", 1);
-
-                  });
-                }
-
-                this.WorkHard = () => {
-                  $rootScope.confirm('Are you sure?', `Are you sure you want to make all workers have 1000% productivity?`, () => {
-                    exports.onNewHour = settings => {
-                    	let allWorkstations = settings.office.workstations;
-                    	allWorkstations.forEach(ws => {
-                    		if (ws.employee != null) {
-                    			ws.employee.speed = 1000
-                    		}
-                    	});
-                    };
-                    $rootScope.addNotification("Made all workers have 1000% productivity.", 1);
-
-                  });
-                }
-
-                this.GoodMood = () => {
-                  $rootScope.confirm('Are you sure?', `Are you sure you want to keep your employees always in good mood?`, () => {
-                    exports.onNewHour = settings => {
-                    	Game.Lifecycle.EmployeeStats.forEach(employeeStat => {
-                    		employeeStat.employee.mood = 100;
-                    	});
-                    };
-                    $rootScope.addNotification("Made all workers have 1000% productivity.", 1);
-
-                  });
-                }
-
-
+      {
+        name: 'sandbox',
+        viewPath: _modPath + 'view.html',
+        controller: function ($rootScope) {
+          this.giveMoney = (amount) => {
+            GetRootScope().confirm('Are you sure?', `Are you sure you want ${numeral(amount).format(Configuration.CURRENCY_FORMAT)}?`, () => {
+              $rootScope.settings.balance += amount
+              $rootScope.addNotification("You 'borrowed' " + numeral(amount).format(Configuration.CURRENCY_FORMAT) + " !", 1)
             }
+          )
         }
-    ]
-};
-Helpers.ConsoleInfo('[MOD] Sandbox menu loaded!');
 
+        this.giveXP = (amount) => {
+          $rootScope.confirm('Are you sure?', `Are you sure you want ${(amount)}XP ?`, () => {
+            $rootScope.settings.xp += amount
+            $rootScope.addNotification("Added " + amount + " XP!", 1);
+          }
+        )
+      }
 
-exports.onLoadGame = settings => {
-  Helpers.ConsoleInfo('I love lasagna');
+        this.giveMaxTire = () => {
+          $rootScope.confirm('Are you sure?', `Are you sure you want get max tire (xp) ?`, () => {
+            Cheats.GetMaxTier()
+            $rootScope.addNotification("Max tier given.", 1);
+          }
+        )
+      }
+
+        this.NextDay = () => {
+          $rootScope.confirm('Are you sure?', `Are you sure you want to skip a day?`, () => {
+            Cheats.SkipADay()
+            $rootScope.addNotification("You skipped one day.", 1);
+          }
+        )
+      }
+
+        this.InventoryGive = () => {
+          $rootScope.confirm('Are you sure?', `Are you sure you want to add 500 items of every item.`, () => {
+            Components.forEach(t=>{
+              GetRootScope().settings.inventory[t.name] += 500
+            })
+            $rootScope.addNotification("Added 500 of every item to your inventory.", 1);
+          }
+        )
+      }
+
+    }
+  }
+]
 };
+
+Helpers.ConsoleInfo('[MOD] Sandbox: Menu loaded!');
+exports.onLoadGame = settings => {};
 exports.onNewHour = settings => {};
-
-///
-exports.onNewDay = settings => {
-let currentDays = SettingsHelper.getValueOrDefault(settings, 'daysSinceInstall', 0);
-
-  currentDays++;
-
-//  if(currentDays = 5) {
-// GetRootScope().SendMail()
-// Helpers.ConsoleInfo('Thank you email sent.');
-//  };
-
-//  SettingsHelper.setValue(settings, 'daysSinceInstall', currentDays);
-};
-
+exports.onNewDay = settings => {};
 exports.onUnsubscribe = done => {
-    Helpers.ConsoleInfo(`[MOD] Sandbox: User has just unsubscribed from this mod... :( `);
-    done();
+  Helpers.ConsoleInfo(`[MOD] Sandbox: User has just unsubscribed from this mod... :( `);
+  done();
 };
