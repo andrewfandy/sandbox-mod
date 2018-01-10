af2eecd9ae3c6d1bf46d71b1a1202184a12f70c6
Aurghyadip <adkundu@gmail.com>
2018-01-10 19:40:29 +0530

    added a check for inventory which fixes NaN problem, fixed indentations removed unwanted codes

--- start.js
+++ start.js
@@ -10,88 +10,69 @@ GetRootScope().OpenMod = () => Remote.app.openLink('http://steamcommunity.com/sh
 Helpers.ConsoleInfo('[MOD] Sandbox: Helper loaded!');
 
 
-exports.initialize = (modPath) =>{
-    _modPath = modPath;
-    // Add a framework
-    /*Frameworks.push(
-      {
-        cuPerMs: 0.1,
-        licenseCost: 0,
-        name: 'Electron',
-        order: 0,
-        pricePerUser: 1
-      }
-    );
-    Helpers.ConsoleInfo('[MOD] Sandbox: Pal framework loaded!');*/
+exports.initialize = (modPath) => {
+	_modPath = modPath;
 
-    // Add new menu item
-    Modding.setMenuItem({
-        name: 'sandbox',
-        tooltip: "Sandbox Mod",
-        tooltipPosition: 'top',
-        faIcon: 'fa-terminal',
-        badgeCount: 0,
-    });
+	// Add new menu item
+	Modding.setMenuItem({
+		name: 'sandbox',
+		tooltip: "Sandbox Mod",
+		tooltipPosition: 'top',
+		faIcon: 'fa-terminal',
+		badgeCount: 0,
+	});
 
-    // Define custom views
-    exports.views = [
-      {
-        name: 'sandbox',
-        viewPath: _modPath + 'view.html',
-        controller: function ($rootScope) {
-          this.giveMoney = (amount) => {
-            GetRootScope().confirm('Are you sure?', `Are you sure you want ${numeral(amount).format(Configuration.CURRENCY_FORMAT)}?`, () => {
-              $rootScope.settings.balance += amount
-              $rootScope.addNotification("You 'borrowed' " + numeral(amount).format(Configuration.CURRENCY_FORMAT) + " !", 1)
-            }
-          )
-        }
+	// Define custom view
+	exports.views = [{
+		name: 'sandbox',
+		viewPath: _modPath + 'view.html',
+		controller: function ($rootScope) {
+			this.giveMoney = (amount) => {
+				GetRootScope().confirm('Are you sure?', `Are you sure you want ${numeral(amount).format(Configuration.CURRENCY_FORMAT)}?`, () => {
+					$rootScope.settings.balance += amount
+					$rootScope.addNotification("You 'borrowed' " + numeral(amount).format(Configuration.CURRENCY_FORMAT) + " !", 1)
+				});
+			}
 
-        this.giveXP = (amount) => {
-          $rootScope.confirm('Are you sure?', `Are you sure you want ${(amount)}XP ?`, () => {
-            $rootScope.settings.xp += amount
-            $rootScope.addNotification("Added " + amount + " XP!", 1);
-          }
-        )
-      }
+			this.giveXP = (amount) => {
+				$rootScope.confirm('Are you sure?', `Are you sure you want ${(amount)}XP ?`, () => {
+					$rootScope.settings.xp += amount
+					$rootScope.addNotification("Added " + amount + " XP!", 1);
+				});
+			}
 
-        this.giveMaxTire = () => {
-          $rootScope.confirm('Are you sure?', `Are you sure you want get max tire (xp) ?`, () => {
-            Cheats.GetMaxTier()
-            $rootScope.addNotification("Max tier given.", 1);
-          }
-        )
-      }
+			this.giveMaxTire = () => {
+				$rootScope.confirm('Are you sure?', `Are you sure you want get max tire (xp) ?`, () => {
+					Cheats.GetMaxTier()
+					$rootScope.addNotification("Max tier given.", 1);
+				});
+			}
 
-        this.NextDay = () => {
-          $rootScope.confirm('Are you sure?', `Are you sure you want to skip a day?`, () => {
-            Cheats.SkipADay()
-            $rootScope.addNotification("You skipped one day.", 1);
-          }
-        )
-      }
+			this.NextDay = () => {
+				$rootScope.confirm('Are you sure?', `Are you sure you want to skip a day?`, () => {
+					Cheats.SkipADay()
+					$rootScope.addNotification("You skipped one day.", 1);
+				});
+			}
 
-        this.InventoryGive = () => {
-          $rootScope.confirm('Are you sure?', `Are you sure you want to add 500 items of every item.`, () => {
-            Components.forEach(t=>{
-              if(GetRootScope().settings.inventory[t.name] > 0)
-                GetRootScope().settings.inventory[t.name] += 500
-            })
-            $rootScope.addNotification("Added 500 of every item to your inventory.", 1);
-          }
-        )
-      }
-
-    }
-  }
-]
-};
+			this.InventoryGive = () => {
+				$rootScope.confirm('Are you sure?', `Are you sure you want to add 500 items of every item.`, () => {
+					Components.forEach(t => {
+						// Check if the inventory item is researched and built once,
+						// solves the NaN problem
+						if(GetRootScope().settings.inventory[t.name] > 0)
+						GetRootScope().settings.inventory[t.name] += 500
+					});
+					$rootScope.addNotification("Added 500 of every item to your inventory.", 1);
+				});
+			}
+		}
+	}]
+}
 
 Helpers.ConsoleInfo('[MOD] Sandbox: Menu loaded!');
-// exports.onLoadGame = settings => {};
-// exports.onNewHour = settings => {};
-// exports.onNewDay = settings => {};
+
 exports.onUnsubscribe = done => {
-  Helpers.ConsoleInfo(`[MOD] Sandbox: User has just unsubscribed from this mod... :( `);
-  done();
-};
+	Helpers.ConsoleInfo(`[MOD] Sandbox: User has just unsubscribed from this mod... :( `);
+	done();
+}
